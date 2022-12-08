a77ad560898600c82af54f61840ff4ecc78ac8b8
Sanjay <Sanjay@DESKTOP-770O1EP>
2022-12-08 12:27:26 +0530

    Pushing to jenkins repo

---
+++ Sample_Maven_Project/src/test/java/sample/DummyTest.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class DummyTest {
+	@Test
+	public void dummy() {
+		System.out.println("dummt test");
+	}
+
+}
---
+++ Sample_Maven_Project/src/test/java/sample/MavenDemoTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class MavenDemoTest {
+	@Test
+	
+	public void Demo() {
+		System.out.println("Welcome to Maven");
+	}
+
+}
